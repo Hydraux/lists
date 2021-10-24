2f7a732d346312ffe24df849f53b59ce874b6190
Hydraux <ryan.bezold@gmail.com>
2021-10-24 02:08:44 -0400

    SafeArea

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -9,45 +9,49 @@ import 'package:lists/views/shopping_list.dart';
 class DashboardPage extends GetView<DashboardController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Obx(() => IndexedStack(
-            index: controller.selectedIndex.value,
-            children: [
-              ShoppingList(),
-              RecipesPage(),
-            ],
-          )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            controller.selectedIndex.value == 0
-                ? Get.find<ItemsController>(tag: 'shoppingList').addItem()
-                : Get.find<RecipesController>().createRecipe();
-          },
-          child: Icon(Icons.add)),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Obx(
-          () => Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                onPressed: () => controller.onTap(0),
-                icon: Icon(
-                  Icons.shopping_cart,
-                  color: controller.selectedIndex.value == 0
+    return SafeArea(
+      bottom: true,
+      top: false,
+      child: Scaffold(
+        body: Obx(() => IndexedStack(
+              index: controller.selectedIndex.value,
+              children: [
+                ShoppingList(),
+                RecipesPage(),
+              ],
+            )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              controller.selectedIndex.value == 0
+                  ? Get.find<ItemsController>(tag: 'shoppingList').addItem()
+                  : Get.find<RecipesController>().createRecipe();
+            },
+            child: Icon(Icons.add)),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          child: Obx(
+            () => Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  onPressed: () => controller.onTap(0),
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: controller.selectedIndex.value == 0
+                        ? Theme.of(context).bottomNavigationBarTheme.selectedItemColor
+                        : Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+                  ),
+                ),
+                IconButton(
+                  color: controller.selectedIndex.value == 1
                       ? Theme.of(context).bottomNavigationBarTheme.selectedItemColor
                       : Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+                  onPressed: () => controller.onTap(1),
+                  icon: Icon(Icons.book),
                 ),
-              ),
-              IconButton(
-                color: controller.selectedIndex.value == 1
-                    ? Theme.of(context).bottomNavigationBarTheme.selectedItemColor
-                    : Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
-                onPressed: () => controller.onTap(1),
-                icon: Icon(Icons.book),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
